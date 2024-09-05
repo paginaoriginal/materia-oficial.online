8fbeb4462c2cf3bba756605b07777e8a55a273eb
paginaoriginal <151889555+paginaoriginal@users.noreply.github.com>
2024-09-04 21:51:48 -0300

    Add files via upload

---
+++ whats/js/vcaJXr8632216.js
@@ -0,0 +1,81 @@
+
+    function atomiApplyParams({inputUrl}) {
+      try {
+        console.log(inputUrl)
+        const inputUrlObj = new URL(inputUrl, window.location.origin);
+        const currentPageParams = new URLSearchParams(window.location.search);
+        const inputUrlParams = new URLSearchParams(inputUrlObj.search);
+      
+        // Iterate over all parameters in the current page's URL
+        for (const [key, value] of currentPageParams) {
+          // If the input URL does not already contain the parameter, add it
+          if (!inputUrlParams.has(key)) {
+            inputUrlParams.append(key, value);
+          }
+        }
+      
+        // Construct the final URL
+        const finalUrl = inputUrlObj.origin + inputUrlObj.pathname + '?' + inputUrlParams.toString();
+        console.log(finalUrl)
+        return finalUrl;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    function atomiFormatDate(options = { slated: false, addDate: 0 }) {
+      try {
+        const defaultOptions = {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        };
+
+        const today = new Date();
+
+        if (options.slated) {
+          const slatedDate = new Date(today);
+          slatedDate.setDate(slatedDate.getDate() + (options.addDate || 0));
+
+          const day = slatedDate.getDate().toString().padStart(2, "0");
+          const month = (slatedDate.getMonth() + 1).toString().padStart(2, "0");
+          const year = slatedDate.getFullYear();
+          return `${day}/${month}/${year}`;
+        }
+
+        if(options.addDate){
+          today.setDate(today.getDate()+options.addDate)
+        }
+        const formattedDate = today.toLocaleDateString(undefined, defaultOptions);
+
+        return formattedDate;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    function atomiFormatTime() {
+      try {
+        const now = new Date();
+        return now.toLocaleTimeString(undefined, {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    function runDelayedFunctions(data) {
+      try {
+        document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+        if(data?.setDisplayed){
+          localStorage.setItem(data?.setDisplayed, true);
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
